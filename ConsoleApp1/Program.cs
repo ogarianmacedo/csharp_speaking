8d72d823b8f160ea2b597f72464ebf6d02b86ee3
Ogarian Macedo <ogarianmetal@gmail.com>
2021-06-01 20:33:04 -0300

    Criação do projeto no GitHub

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Speech.Synthesis;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            var builder = new PromptBuilder();
+            builder.StartVoice(new CultureInfo("pt-BR"));
+            builder.AppendText("Olá, digite seu nome por favor.");
+            builder.EndVoice();
+            synthesizer.Speak(builder);
+
+            string textoA = "";
+            Console.WriteLine("Aguardando resposta...");
+            textoA = Convert.ToString(Console.ReadLine());
+
+            if (textoA != null)
+            {
+                var builderA = new PromptBuilder();
+                builderA.StartVoice(new CultureInfo("pt-BR"));
+                builderA.AppendText("Olá " + textoA + ", bem-vindo!");
+                builderA.EndVoice();
+                synthesizer.Speak(builderA);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# csharp_speaking
+
+Projeto Livre - ConsoleApp com uso do SpeechSynthesizer\
+para geração de fala síncrona a partir de caracteres.
